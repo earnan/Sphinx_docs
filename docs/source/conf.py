19b922e89f8a069fb41a4f219263fdb329b18194
earnan <hi@arcsona.cn>
2022-04-12 17:24:13 +0800

    1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -51,7 +51,7 @@ extensions = [
         'sphinx.ext.ifconfig',
         'sphinx.ext.viewcode',
         'sphinx.ext.githubpages',
-        #'sphinxcontrib.fulltoc',
+        'sphinxcontrib.fulltoc',
 ]
 
 # Add any paths that contain templates here, relative to this directory.

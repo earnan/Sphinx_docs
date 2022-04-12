134749fa8d5567613baf840f42212b088e448e5e
earnan <hi@arcsona.cn>
2022-04-12 17:01:14 +0800

    diyciitijiao

---
+++ src/run.py
@@ -0,0 +1,15 @@
+def dummy_print(something):
+    """
+   a print function which is the same as the original func
+
+   :param str something: a str you want to print
+   :return: no return
+
+   这是一个简单的例子：
+
+   Example
+   -------
+   >>> dummy_print("hello")
+   hello
+   """
+    print(something)
